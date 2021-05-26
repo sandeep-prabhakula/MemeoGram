78ff678f23e3f99f43af566a99dac5065b029378
sandeep <sandeepprabhakula10@gmail.com>
2021-05-26 20:40:45 +0530

    Removed Action Bar in splash Screen

--- app/src/main/java/com/example/memeshare/SplashScreen.java
+++ app/src/main/java/com/example/memeshare/SplashScreen.java
@@ -6,13 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import java.util.Objects;
+
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        
         new Handler().postDelayed(()->{
             startActivity(new Intent(SplashScreen.this,MainActivity.class));
             finish();
